9b514954b6adb565b6d42394817ea4a8508716db
pythonowiec <kuba.dobosz2004@gmail.com>
2021-02-07 18:17:19 +0100

    project-finished

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 const content = document.getElementById("content");
 
-const image = document.getElementById("bug");
+const image = document.getElementById("image");
+
+var snd = new Audio("plik.mp3");
+snd.loop = true;
+snd.autoplay = true;
+snd.volume = 0.3;
+
+
+var snd2 = new Audio("plik2.mp3");
+snd2.loop = true;
+snd2.autoplay = true;
+snd2.volume = 0.35;
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -23,5 +36,14 @@ document.addEventListener("DOMContentLoaded", () => {
         fill: "forwards"
     }
     );
+
+    table.animate([
+        { opacity: 1 } 
+     ],
+     {
+         duration: 1000,
+         fill: "forwards"
+     }
+     );
 
 });
